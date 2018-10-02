f8473ad8b01a7f43057e17dce4df9b75a7f9fd9f
Zac Chan <sunshinecltzac@gmail.com>
2018-10-01 21:03:04 -0400

    update

--- HW1/assignment/2_pytorch/models/mymodel.py
+++ HW1/assignment/2_pytorch/models/mymodel.py
@@ -9,7 +9,7 @@ cfg = {
     'VGG13': [64, 64, 'M', 128, 128, 'M', 256, 256, 'M', 512, 512, 'M', 512, 512, 'M'],
     'VGG16': [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 'M', 512, 512, 512, 'M', 512, 512, 512, 'M'],
     'VGG19': [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 256, 'M', 512, 512, 512, 512, 'M', 512, 512, 512, 512, 'M'],
-    'VGG_my': [64, 64, 'M', 128, 128, 'M']
+    'VGG_my': [64, 64, 'M', 128, 128, 'M', 256, 256, 'M']
 }
 
 class MyModel(nn.Module):
@@ -27,9 +27,9 @@ class MyModel(nn.Module):
         #############################################################################
         # TODO: Initialize anything you need for the forward pass
         #############################################################################
-        self.dropout = nn.Dropout(p=0.25)
-        self.features = self._make_layers(cfg["VGG13"])
-        self.classifier = nn.Linear(512, 10)
+        self.dropout = nn.Dropout(p=0.5)
+        self.features = self._make_layers(cfg["VGG_my"])
+        self.classifier = nn.Linear(8192, 10)
         #############################################################################
         #                             END OF YOUR CODE                              #
         #############################################################################
@@ -67,8 +67,7 @@ class MyModel(nn.Module):
         in_channels = 3
         for x in cfg:
             if x == 'M':
-                layers += [nn.MaxPool2d(kernel_size=2, stride=2),
-                           nn.Dropout2d(p=0.25)]
+                layers += [nn.MaxPool2d(kernel_size=2, stride=2)]
             else:
                 layers += [nn.Conv2d(in_channels, x, kernel_size=3, padding=1),
                            nn.BatchNorm2d(x),
